e10f0b8858d0eb64b53982073032078f6fd47557
vahanalasumamoulika <sumamoulika.v@terralogic.com>
2022-10-17 15:36:23 +0530

    change

---
+++ sumagit/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  sumagit
+//
+//  Created by SumaMoulika V-3000 on 17/10/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("suma")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
