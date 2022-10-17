91f03b9d39a634aeb40c624a8a388a328fa2de88
vahanalasumamoulika <sumamoulika.v@terralogic.com>
2022-10-17 15:35:31 +0530

    Initial Commit

---
+++ sumagit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sumagit
+//
+//  Created by SumaMoulika V-3000 on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
